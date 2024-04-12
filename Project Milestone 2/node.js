2ee38416fd3acbc42edb169654b08cfb253d75e1
jeso04 <brandjesse013@gmail.com>
2024-04-12 11:38:34 -0400

    Project Milestone 2

--- Project Milestone 2/node.js
+++ Project Milestone 2/node.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({ extnded: true })); // Here is where applications will be pass to
+app.use(express.urlencoded({ extended: true })); // Here is where applications will be pass to
 
 app.get('/', (req, res) => {
-    res.sendfile(__dirname + '/index.html'); // 'index.html' will be sent upon the 'get' request
+    res.sendFile(__dirname + '/index.html'); // 'index.html' will be sent upon the 'get' request
 });
 
 app.post('/submit-form', (req, res) => {
-    const Your_Name = req.body.username; //Will be used to access form data
+    const Your_Name = req.body.Your_Name; //Will be used to access form data
     // Valid name will be added here
     res.send('Your name is Jesse Brand');
 });
 
 app.listen(port, () => {
-    console.log('Server running on http://localhost:${port}'); // Tells to run on the local host
+    console.log(`Server running on http://localhost:${port}`); // Tells to run on the local host
 });
