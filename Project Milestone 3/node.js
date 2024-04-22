41fddc453405a057420fc9be25d2664f2dbdbf0d
jeso04 <brandjesse013@gmail.com>
2024-04-22 17:29:32 -0400

    Project Milestone 3

--- Project Milestone 3/node.js
+++ Project Milestone 3/node.js
@@ -5,11 +5,11 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true })); // Here is where applications will be pass to
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html'); // 'index.html' will be sent upon the 'get' request
+    res.sendFile(__dirname + 'index.html'); // 'index.html' will be sent upon the 'get' request
 });
 
 app.post('/submit-form', (req, res) => {
-    const Your_Name = req.body.Your_Name; //Will be used to access form data
+    const Name = req.body.Name; //Will be used to access form data
     // Valid name will be added here
     res.send('Your name is Jesse Brand');
 });
